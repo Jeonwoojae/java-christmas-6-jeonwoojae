57317044e56fa40e75f6a8feabb1cec4586a9524
Jeonwoojae <dnwo0719@icloud.com>
2023-11-13 17:37:38 +0900

    refactor: 나눠진 예외처리를 통합 처리

--- src/main/java/christmas/view/InputView.java
+++ src/main/java/christmas/view/InputView.java
@@ -14,10 +14,8 @@ public class InputView {
         Integer convertedValue;
         try {
             convertedValue = Integer.parseInt(value);
-        } catch (NumberFormatException e) {
-            throw new IllegalArgumentException("[ERROR] 입력값이 숫자 형식이 아닙니다.");
-        } catch (NoSuchElementException e) {
-            throw new IllegalArgumentException("[ERROR] 빈 값을 입력할 수 없습니다.");
+        } catch (NumberFormatException | NoSuchElementException e) {
+            throw new IllegalArgumentException("[ERROR] 유효하지 않은 날짜입니다. 다시 입력해 주세요.");
         }
         return convertedValue;
     }

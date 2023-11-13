f62178c89bb09dfd6b17907f56ea7b40b11ce6ab
Jeonwoojae <dnwo0719@icloud.com>
2023-11-14 00:36:33 +0900

    refactor: 특수 날짜 조건을 저장할 클래스명을 Config로 변경

--- src/main/java/christmas/constants/Config.java
+++ src/main/java/christmas/constants/Config.java
@@ -4,7 +4,7 @@ import christmas.dto.Day;
 import java.util.Arrays;
 import java.util.List;
 
-public class SpecialDays {
+public class Config {
     private static final Integer[] SPECIAL_DAYS_INFO = {3, 10, 17, 24, 25, 31};
     public static final List<Day> SPECIAL_DAYS = Arrays.stream(SPECIAL_DAYS_INFO).map(Day::new).toList();
 }
--- src/main/java/christmas/domain/discount/SpecialDiscountStrategy.java
+++ src/main/java/christmas/domain/discount/SpecialDiscountStrategy.java
@@ -1,7 +1,7 @@
 package christmas.domain.discount;
 
 import christmas.domain.Order;
-import christmas.constants.SpecialDays;
+import christmas.constants.Config;
 import christmas.dto.Day;
 import christmas.dto.DiscountInfo;
 import christmas.dto.Money;
@@ -10,7 +10,7 @@ public class SpecialDiscountStrategy implements DateDiscountStrategy{
     @Override
     public DiscountInfo calculateDiscountAmount(Order order, Day day) {
         Money discountAmount = new Money(0);
-        if (SpecialDays.SPECIAL_DAYS.contains(day)) {
+        if (Config.SPECIAL_DAYS.contains(day)) {
             discountAmount = new Money(1_000L);
         }
         return new DiscountInfo(Event.SPECIAL_DISCOUNT, discountAmount);

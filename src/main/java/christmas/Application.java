fc611e0ab337d5cb8503f3bd60a11e799d047e57
Jeonwoojae <dnwo0719@icloud.com>
2023-11-14 21:36:45 +0900

    feat: Application 종료 시 콘솔을 닫도록 구현

--- src/main/java/christmas/Application.java
+++ src/main/java/christmas/Application.java
@@ -1,10 +1,12 @@
 package christmas;
 
+import camp.nextstep.edu.missionutils.Console;
 import christmas.controller.EventPlannerController;
 
 public class Application {
     public static void main(String[] args) {
         EventPlannerController controller = new EventPlannerController();
         controller.run();
+        Console.close();
     }
 }
